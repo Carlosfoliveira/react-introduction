2223324564a43532b848caf521a037ed1d68392a
carlosfoliveira <carlos.fe.oliveira@gmail.com>
2020-07-19 15:56:13 -0300

    12 - Ciclo de vida do componente

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -4,12 +4,28 @@ import TechItem from './TechItem';
 class TechList extends Component {
   state = {
     newTech: '',
-    techs: [
-      'Node.js',
-      'ReactJS',
-      'React Native'
-    ]
+    techs: []
   };
+
+  // Executado assim que o componente aparece em tela
+  componentDidMount() {
+    const techs = localStorage.getItem('techs');
+
+    if (techs) {
+      this.setState({ techs: JSON.parse(techs) });
+    }
+  }
+
+  // Executado sempre que houver alterações nas props ou estado
+  componentDidUpdate(_, prevState) {
+    if (prevState.techs !== this.state.techs) {
+      localStorage.setItem('techs', JSON.stringify(this.state.techs));
+    }
+  }
+
+  // Executado quando o componente deixa de existir
+  componentWillUnmount() {
+  }
 
   handleInputChange = e => {
     this.setState({ newTech: e.target.value });
@@ -32,7 +48,6 @@ class TechList extends Component {
       <form onSubmit={this.handleSubmit}>
         <ul>
           {this.state.techs.map(tech => <TechItem key={tech} tech={tech} onDelete={() => this.handleDelete(tech)} />)}
-          <TechItem />
         </ul>
         <input
           type="text"
